1d445cd919cd891d44c53e2c981ea530014dbfd8
Elwood Steven Berry III <me@elwoodberry.com>
2017-04-21 07:21:50 -0400

    Added core routing and SASS files

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # elwoodberry
-elwoodberry.com
+An Angular 2.2 (stable) Single Page Application(SPA) for www.elwoodberry.com
+
+### Project Backlog  
+Solution: **Pivotal Tracker** [Launch Project Backlog](https://www.pivotaltracker.com/n/projects/1940425)  
---
+++ server.js
@@ -0,0 +1,33 @@
+// Dependencies
+var express = require('express');  
+var path = require('path');  
+var bodyParser = require('body-parser'); 
+
+// Routes
+var index = require('./routes/index');
+
+// Port
+var port;
+
+// Express
+var app = express(); 
+
+// Body Parser
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({extended: false}));
+
+// Static Route Angular
+app.use(express.static(path.join(__dirname, 'client')));
+
+// Routes
+app.use('/', index);
+
+// 404
+app.use(function (req, res, next) {
+  res.sendFile(__dirname + '/views/404.html');
+})
+
+// Listen to run the server
+app.listen(port, function(){
+	console.log('Available On Root Domain');
+});
